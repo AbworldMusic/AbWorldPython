417604520d2255a7b6b4b8faac4ce6bad3ac18dd
AbWorldMusic <abworldappteam@gmail.com>
2020-06-20 20:18:26 +0530

    Added home pages

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 
 Bootstrap(app)
+
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template("home.html")
+
+@app.route('/enrollment')
+def enrollment():
+    if request.method == "GET":
+        return render_template("enrollment.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
