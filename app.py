51e12f79031821a0d8cd2334468a7954fe7264fb
AbWorldMusic <abworldappteam@gmail.com>
2020-10-28 17:26:00 +0530

    Fixes

--- app.py
+++ app.py
@@ -1343,7 +1343,8 @@ def API_get_slots_for_faculty():
 def API_get_student_list():
     class_id = request.args['class_id']
     cur = mysql.connection.cursor()
-    query = "SELECT student_id from student_slots WHERE slot_id=81"
+    query = "SELECT student_id from student_slots WHERE slot_id="+str(class_id)
+    return  query
     cur.execute(query)
     records = cur.fetch_all()
     all_students = {}
